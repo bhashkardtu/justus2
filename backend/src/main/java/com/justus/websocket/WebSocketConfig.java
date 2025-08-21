4af2dc389e3b1b7d786e2510954f742151cf9ccb
BhashkarKumar2 <bhashkarkumar2063@gmail.com>
2025-08-22 00:59:21 +0530

    deployment initiated

--- backend/src/main/java/com/justus/websocket/WebSocketConfig.java
+++ backend/src/main/java/com/justus/websocket/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000", "http://localhost:3001").withSockJS()
+        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000", "http://localhost:3001","https://just-us-liard.vercel.app").withSockJS()
                 .setInterceptors(new HandshakeAuthInterceptor());
     }
 }
